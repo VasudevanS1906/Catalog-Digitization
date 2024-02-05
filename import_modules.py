56516143dd33aa447388142ae54d6af685dd2ec2
VasudevanS1906 <157589360+VasudevanS1906@users.noreply.github.com>
2024-02-05 21:59:32 +0530

    Create import_modules.py

---
+++ import_modules.py
@@ -0,0 +1,16 @@
+import random
+import string
+import sys
+import json
+from PIL import Image
+import numpy
+import time
+from translate import Translator
+from gtts import gTTS
+from playsound import playsound 
+import os
+import urllib3
+from urlparse import urlparse
+import pytesseract
+import glob
+from bs4 import BeautifulSoup
